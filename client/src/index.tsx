a14e8a48ef687e44f494232aeb60ce4adc257074
wilsuriel03 <lacurly00@gmail.com>
2022-10-05 18:16:14 -0500

    feat: addedd redux store provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./index.css";
 
 import App from "./App";
 
@@ -12,7 +15,9 @@ if (!el) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
