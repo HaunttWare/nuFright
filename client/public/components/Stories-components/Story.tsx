69ee6a532b3e73b319521952fc4e716131ff849f
iamdestinos <iamdestinos@gmail.com>
2022-10-05 16:02:59 -0500

    Add: template for Story subcomponent

--- client/public/components/Stories-components/Story.tsx
+++ client/public/components/Stories-components/Story.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 const Story = () => {
+    const HauntedHouse = require('../../../../assets/haunted-house.jpg').default;
 
     return (
         <div className="row" style={{background: 'rgb(220, 53, 69)', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <h5 style={{display: 'flex', justifyContent: 'center'}}><b><u>Story Title</u></b></h5>
-            <img src='https://www.psychologicalscience.org/redesign/wp-content/uploads/2022/01/How-Haunted-Houses-Measure-Fear-Web-1600x842.jpg' style={{maxHeight: 450, maxWidth: 450, overflow: 'hidden'}}></img>
+            <h5 style={{display: 'flex', justifyContent: 'center', cursor: 'pointer'}}><b><u>Story Title</u></b></h5>
+            <img src={HauntedHouse} style={{maxHeight: 450, maxWidth: 450, overflow: 'hidden'}}></img>
             <div style={{display: 'flex', justifyContent: 'center'}}>
                 Description goes here
             </div>
-            <button style={{maxWidth: 250}}>Read More</button>
+            <button style={{maxWidth: 250, borderRadius: '45%', background: 'black', color: 'lime'}}>Read More</button>
         </div>
     )
 }
