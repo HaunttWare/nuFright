c4ee2841b0a18fbb181891d3c468c21ad6f60292
iamdestinos <iamdestinos@gmail.com>
2022-10-05 16:55:13 -0500

    add: display for selected story

---
+++ client/public/components/Stories-components/StoryDisplay.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StoryDisplay = () => {
+    return (
+        <div style={{background: 'rgb(220, 53, 69)', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <h5 style={{display: 'flex', justifyContent: 'center'}}><b><u>individual story title</u></b></h5>
+        </div>
+    )
+}
+
+export default StoryDisplay;
