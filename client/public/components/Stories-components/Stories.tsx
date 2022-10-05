272a39b4ba4366e649c3a30a0b29457c432d6cde
iamdestinos <iamdestinos@gmail.com>
2022-10-05 16:37:19 -0500

    add: beginning functionality to show individual story

--- client/public/components/Stories-components/Stories.tsx
+++ client/public/components/Stories-components/Stories.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 //subcomponents
 import Story from './Story';
 
 const StoriesPage = () => {
+    const [view, setView] = useState('storyList');
+
     return (
         <div id="stories_page">
-            <Story/>
+            {view === 'storyList' && <Story test={setView} />}
+            {view === 'story' && <div>individual story</div>}
         </div>
     );
 }
