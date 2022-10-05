5d128818c6baeb071794a5c0dc5990bf620a2249
wilsuriel03 <lacurly00@gmail.com>
2022-10-04 20:19:39 -0500

    feat: added necessary navigation routes

--- client/src/routes/navigation/navigation.component.tsx
+++ client/src/routes/navigation/navigation.component.tsx
@@ -1,13 +1,52 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
-const Navigation = () => {
-  return (
-    <>
-<h1>Navigation</h1>
-      <Outlet />
-    </>
-  );
-};
+const Navigation = () => (
+  <>
+    <nav className="navbar navbar-expand-lg bg-danger">
+      <div className="container-fluid text-light">
+        <Link className="navbar-brand" to="/">
+          NuFright
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
+            <li className="nav-item">
+              <Link className="nav-link" to="/movies-shows">
+                Movies/Shows
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/books">
+                Books
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/stories">
+                Stories
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/gallery">
+                Gallery
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <Outlet />
+  </>
+);
 
 export default Navigation;
