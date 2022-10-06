56c3e00b0929adc4d0b68e792791b30ce3427917
itcomesinwaves <royce.taylor789@gmail.com>
2022-10-06 11:53:24 -0500

    passport errors

--- server/routes/auth/auth.ts
+++ server/routes/auth/auth.ts
@@ -1,3 +1,5 @@
+require('dotenv').config();
+import { stringify } from 'querystring';
 import { db } from '../../prisma/utils/db.server';
 
 
@@ -8,13 +10,16 @@ const passport = require('passport');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: "http://localhost:3000/auth/google/callback",
+  scope: ['profile'],
+  state: true
 },
-async function(accessToken: string, refreshToken: string, profile: { id: string, email: string, photos: string, name: string }, cb: Function) {
-  const user: Function = await db.user.upsert({
+async (accessToken: string, refreshToken: string, profile: { id: string, email: string, photos: string, name: string, googleId: any }, cb: Function) => {
+  const user = await db.user.upsert({
     where: {googleId: profile.id},
     update: {},
     create: { googleId: profile.id, email: profile.email, avatar: profile.photos[0], name: profile.name }
   })
 }))
+
 
