dfc750a0f5ee3d2e7b0f1be2d89746da82c093f9
iamdestinos <iamdestinos@gmail.com>
2022-10-06 11:56:48 -0500

    add: WriteStory subcomponent hookup

--- client/public/components/Stories-components/Stories.tsx
+++ client/public/components/Stories-components/Stories.tsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 //subcomponents
 import Story from './Story';
 import StoryDisplay from './StoryDisplay';
+import WriteStory from './WriteStory';
 
 const StoriesPage = () => {
     const [view, setView] = useState('storyList');
 
     return (
         <div id="stories_page">
+            {view === 'storyList' && <button onClick={() => setView('write')} style={{maxWidth: 200, background: 'black', color: 'lime', borderRadius: '45%'}}>Write a Story</button>}
             {view === 'storyList' && <Story test={setView} />}
-            {view === 'story' && <StoryDisplay/>}
+            {view === 'story' && <StoryDisplay backHandler={setView}/>}
+            {view === 'write' && <WriteStory/>}
         </div>
     );
 }
