e4c214c4de59d1d22083c5feb03ebaa4fabcd72b
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-05 22:10:41 -0500

    feat: successfully storing images in the s3 bucket

--- server/routes/images/images.controller.ts
+++ server/routes/images/images.controller.ts
@@ -1,10 +1,50 @@
 import express from 'express';
 import { db } from '../../prisma/utils/db.server';
-
+// multer to handle multipart form data
+import multer from 'multer';
+// s3 client to interact with s3 buckets
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+require('dotenv').config();
 const images = express.Router();
 
-images.post('/', (req, res) => {
-  console.log('here is the body from the image post\n', req.body);
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
+// s3 variables
+const bucketKey = (process.env.BUCKET_KEY as string);
+const bucketSecret = (process.env.BUCKET_SECRET as string);
+const bucketRegion = (process.env.BUCKET_REGION as string);
+const bucketName = (process.env.BUCKET_NAME as string);
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: bucketKey,
+    secretAccessKey: bucketSecret
+  },
+  region: bucketRegion
+});
+
+images.post('/', upload.single('image'), async (req, res) => {
+  // console.log('whats in the body\n', req.body);
+  // console.log('whats in the file\n', req.file);
+  // console.log('whats the bucketname?\n', bucketName)
+  
+
+  const params = {
+    Bucket: bucketName,
+    Key: req.file?.originalname,
+    Body: req.file?.buffer,
+    ContentType: req.file?.mimetype
+  }
+  const command = new PutObjectCommand(params);
+  s3.send(command)
+  .then((data) => {
+    console.log('s3 command successfulllllll');
+  })
+  .catch((err) => {
+    console.error('error on s3 send command\n', err);
+  })
 
   res.sendStatus(200);
 });
