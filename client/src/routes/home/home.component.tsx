5595d72d91623bdbea9d30a38ce975b39af15be4
jimenezfede <ricojimenez89@gmail.com>
2022-10-06 21:03:50 -0400

    rendering haunted house list component

--- client/src/routes/home/home.component.tsx
+++ client/src/routes/home/home.component.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import HauntedHouseList from '../haunted-houses/haunted-house-list.componet'
 
 const Home = () => (
   <div className="container">
     <h1>Home</h1>
+    <HauntedHouseList />
   </div>
 );
 
