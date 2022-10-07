9c1f3f3d13ec96a7fb95cd46fe9185cc26f21398
itcomesinwaves <royce.taylor789@gmail.com>
2022-10-06 20:02:42 -0500

    merge

--- server/routes/auth/auth.ts
+++ server/routes/auth/auth.ts
@@ -1,25 +1,34 @@
-require('dotenv').config();
-import { stringify } from 'querystring';
-import { db } from '../../prisma/utils/db.server';
+// require('dotenv').config();
+// import { Prisma, PrismaClient } from '@prisma/client';
+// import { Profile } from 'passport';
+// import { db } from '../../prisma/utils/db.server';
 
 
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const passport = require('passport');
 
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback",
-  scope: ['profile'],
-  state: true
-},
-async (accessToken: string, refreshToken: string, profile: { id: string, email: string, photos: string, name: string, googleId: any }, cb: Function) => {
-  const user = await db.user.upsert({
-    where: {googleId: profile.id},
-    update: {},
-    create: { googleId: profile.id, email: profile.email, avatar: profile.photos[0], name: profile.name }
-  })
-}))
+// passport.use(new GoogleStrategy({
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   callbackURL: "http://localhost:3000/auth/google/callback",
+//   scope: ['profile'],
+//   state: true
+// },
+// async (accessToken: string, refreshToken: string, profile: Profile, cb: Function) => {
+//   // const user = await db.user.upsert({
+//   //   where: {googleId: profile.id},
+//   //   update: {},
+//   //   create: { googleId: profile.id, email: profile.email, avatar: profile.photos[0], name: profile.name }
+//   // })
+
+//   const user = await db.user.findUnique({
+//     where: {googleId: profile.id}
+//   })
+  
+//   if (!user) {
+
+//   }
+// }))
 
 
