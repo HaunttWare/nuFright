3d2361d4a2f308ffdb42550b7d658d64e8629340
wilsuriel03 <lacurly00@gmail.com>
2022-10-06 21:04:05 -0500

    feat: added endpoints for auth

---
+++ server/routes/auth/auth.router.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import passport from "passport";
+
+const authRouter = express.Router();
+
+authRouter.get("/login/successful", (req, res) => {
+  if (req.user) {
+    res.json({
+      success: true,
+      message: "user has successfully authenticated",
+      user: req.user,
+      // cookies: req.cookies
+    });
+  }
+});
+
+authRouter.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "user failed to authenticate.",
+  });
+});
+
+authRouter.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+authRouter.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/login/failed",
+  })
+);
+
+authRouter.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      res.send(err);
+    }
+  });
+  res.redirect("/");
+});
+
+export default authRouter;
