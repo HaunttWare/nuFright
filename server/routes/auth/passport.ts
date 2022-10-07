3f7caa06a542f63a7ff7de32c0c3f17a93d24dc6
itcomesinwaves <royce.taylor789@gmail.com>
2022-10-07 11:24:09 -0500

    Co-authored-by: erincwilliams0 <erincwilliams0@users.noreply.github.com>

--- server/routes/auth/passport.ts
+++ server/routes/auth/passport.ts
@@ -34,9 +34,13 @@ passport.use(
 );
 
 passport.serializeUser((user, done) => {
+
   done(null, user);
 });
 
-passport.deserializeUser((user: any, done) => done(null, user));
+passport.deserializeUser((user: any, done) => {
+  console.log(user);
+  done(null, user);
+});
 
 export default passport;
