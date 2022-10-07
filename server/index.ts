9c3f5838cca493c268d34489c3ea9b5cdd5ea5c9
wilsuriel03 <lacurly00@gmail.com>
2022-10-06 21:05:24 -0500

    feat: added middleware cors,  initialized passport, and imported root router

--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,46 @@
 import express, { Request, Response } from "express";
-const path = require('path');
-const images = require('./routes/images/images.controller.ts');
+import path from "path";
+import cors from "cors";
+import passport from "passport";
+import cookieSession from "cookie-session";
+import "./routes/auth/passport";
+
+import rootRouter from "./routes";
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.resolve('client', 'public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve("client", "public")));
 
-app.use('/images', images);
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1"],
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use("/", rootRouter);
 
 // route to handle all other endpoints and server index.html
-app.get('/*', (req: Request, res: Response) => {
-  res.sendFile(path.resolve('client', 'public', 'index.html'));
+app.get("/*", (req: Request, res: Response) => {
+  res.sendFile(path.resolve("client", "public", "index.html"));
 });
 
 app.listen(PORT, () => {
   console.log(`⚡[server]: Server is running at http://localhost:${PORT}`);
-})
+});
