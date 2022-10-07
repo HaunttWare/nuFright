0e27df0fb4151cb10fdea400428edeb7d36dc1b2
itcomesinwaves <royce.taylor789@gmail.com>
2022-10-07 00:26:54 -0500

    merge

--- client/src/routes/home/home.component.tsx
+++ client/src/routes/home/home.component.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => (
+  const [user, setUser] = useState<User>({} as User);
+
+handleLogin = () => {
+
+}
+handleLogout = () => {
+
+}
+
+return (
+  user ? 
+  <div className="container">
+    <h1>Home Page logged in</h1>
+    <button onClick={handleLogout}>logout</button>  
+  </div>
+  :
   <div className="container">
     <h1>Home</h1>
+    <button onClick={handleLogin}>Login</button>
   </div>
+)
 );
 
 export default Home;
