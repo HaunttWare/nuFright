de8db6034fb09dcef8efb32fe6cc0050b080812c
jimenezfede <ricojimenez89@gmail.com>
2022-10-07 10:27:21 -0400

    added location property

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger'; 
 
 import { userReducer } from './user/user.reducer';
+import { locationReducer } from './location/location.reducer';
 
 export const store = configureStore({ // <-- this function creates the store 
     reducer: {
-        user: userReducer // <-- this sets the user reducer in the store
+        user: userReducer, // <-- this sets the user reducer in the store
+        location: locationReducer
     },
     middleware: (process.env.NODE_ENV === 'development' ? [logger] : []) // <-- this adds the logger middleware only in development mode
 });
