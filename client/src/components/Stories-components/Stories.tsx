b121f74ddd0a68d1f84d04736e8baf26b597965b
iamdestinos <iamdestinos@gmail.com>
2022-10-08 13:33:37 -0500

    update: hook up storylist to live data

--- client/src/components/Stories-components/Stories.tsx
+++ client/src/components/Stories-components/Stories.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 //subcomponents
 import Story from './Story';
 import StoryDisplay from './StoryDisplay';
@@ -6,13 +7,34 @@ import WriteStory from './WriteStory';
 
 const StoriesPage = () => {
     const [view, setView] = useState('storyList');
+    const [allStories, setAllStories] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    //on initializing, get all stories from database
+    if(isLoading) {
+        axios.get('/story/allStories')
+        .then(result => {
+            setAllStories(result.data);
+        })
+        .catch((err:Error) => console.error(err));
+        setIsLoading(false);
+    }
+
+    //view handler handles changing views
+    const viewHandler = (view:any, props:any) => {
+        setView(view);
+    }
 
     return (
         <div id="stories_page">
             {view === 'storyList' && <button onClick={() => setView('write')} style={{maxWidth: 200, background: 'black', color: 'lime', borderRadius: '45%'}}>Write a Story</button>}
-            {view === 'storyList' && <Story test={setView} />}
-            {view === 'story' && <StoryDisplay backHandler={setView}/>}
-            {view === 'write' && <WriteStory backHandler={setView}/>}
+            <>
+            {view === 'storyList' && allStories.map(((story:{authorId:String, createdAt:String, id:String, images:any, title:String, story:String}, index:any) => {
+                return <Story test={viewHandler} story={story} key={index} />
+            }))}
+            </>
+            {view === 'story' && <StoryDisplay backHandler={viewHandler}/>}
+            {view === 'write' && <WriteStory backHandler={viewHandler}/>}
         </div>
     );
 }
