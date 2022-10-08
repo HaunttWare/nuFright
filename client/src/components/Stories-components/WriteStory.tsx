b14adf90ccf3bf4a3b43d9d007d7021ace65eb11
iamdestinos <iamdestinos@gmail.com>
2022-10-08 15:56:09 -0500

    update: loading indicator for post

--- client/src/components/Stories-components/WriteStory.tsx
+++ client/src/components/Stories-components/WriteStory.tsx
@@ -7,6 +7,8 @@ const WriteStory = (props:{backHandler: Function}) => {
     const currentUser = useSelector(selectCurrentUser);
     const [title, setTitle] = useState('');
     const [text, setText] = useState('');
+    const [formFilled, setFormFilled] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     //handlers for title and text
     const titleHandler = (event: any) => {
@@ -20,9 +22,20 @@ const WriteStory = (props:{backHandler: Function}) => {
     //publish handler
     const postHandler = () => {
         if(title && text && currentUser) {
+            setIsLoading(true);
+            setFormFilled('Posting...');
             axios.post('/story/addStory', {userId: currentUser.id, title: title, text: text})
-            .then((result:any) => props.backHandler('storyList'))
-            .catch((err:Error) => console.error(err));
+            .then((result:any) => {
+                props.backHandler('storyList')
+                setIsLoading(false);
+            })
+            .catch((err:Error) => {
+                console.error(err)
+                setIsLoading(false);
+                setFormFilled('Error posting story. Please try again.');
+            });
+        } else {
+            setFormFilled('Write in all fields in order to post your story!');
         }
     }
 
@@ -32,7 +45,8 @@ const WriteStory = (props:{backHandler: Function}) => {
             <br style={{margin: 5}}></br>
             <input placeholder='Write your title here...' onChange={titleHandler} value={title} style={{width: '100%', display: 'block', marginBottom: 5}}></input>
             <textarea rows={5} placeholder='Write your story here...' onChange={textHandler} value={text} style={{width: '100%', display: 'block', marginBottom: 5}}></textarea>
-            <button onClick={postHandler} style={{background: 'black', color: 'lime', borderRadius: '45%', minWidth: 50}}>Post</button>
+            <p><b>{formFilled}</b></p>
+            <button disabled={isLoading} onClick={postHandler} style={{background: 'black', color: 'lime', borderRadius: '45%', minWidth: 50}}>Post</button>
         </div>
     )
 }
