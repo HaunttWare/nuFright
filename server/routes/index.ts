1f30163b3c65f692fd8fe88dafbed5213e3e6cdc
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-07 22:57:34 -0500

    feat: temporarily included images in root router

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
 import authRouter from "./auth/auth.router";
+import images from "./images/images.controller";
 
 const rootRouter = express.Router();
 
 rootRouter.use("/auth", authRouter);
+rootRouter.use('/images', images);
 
 export default rootRouter;
 
