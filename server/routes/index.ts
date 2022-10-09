d7a6c6e4bf03f6b865a53d7baba13116619fa4b9
wilsuriel03 <lacurly00@gmail.com>
2022-10-08 22:15:36 -0500

    add books endpoint

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,11 +2,14 @@ import express from "express";
 
 import authRouter from "./auth/auth.router";
 import storyRouter from './stories/stories.router';
+import booksRouter from "./books/books.router";
 
 const rootRouter = express.Router();
 
 rootRouter.use("/auth", authRouter);
 rootRouter.use('/story', storyRouter);
+rootRouter.use("/books", booksRouter);
+
 
 export default rootRouter;
 
