f40eb022d436076900303e0c7ed9b7c18bfa6862
wilsuriel03 <lacurly00@gmail.com>
2022-10-08 22:10:24 -0500

    feat: add books to state

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger'; 
 
 import { userReducer } from './user/user.reducer';
+import { booksReducer } from './books/books.reducer';
 
 export const store = configureStore({ // <-- this function creates the store 
     reducer: {
         user: userReducer, // <-- this sets the user reducer in the store
+        books: booksReducer, // <-- this sets the books reducer in the store
     },
     middleware: (process.env.NODE_ENV === 'development' ? [logger] : []) // <-- this adds the logger middleware only in development mode
 });
