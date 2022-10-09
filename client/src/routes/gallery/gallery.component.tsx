a0afee9e70b242dc588a504e0542d2123cb1d7f4
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-08 23:15:38 -0500

    feat: images set in state, setting up bootstrap spinner

--- client/src/routes/gallery/gallery.component.tsx
+++ client/src/routes/gallery/gallery.component.tsx
@@ -1,28 +1,37 @@
+import { Images } from ".prisma/client";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ImagePost from '../../components/Gallery-components/galleryPost.component';
 import ImageCard from '../../components/Gallery-components/ImageCard.component';
+import loadSpinner from '../../components/Gallery-components/loadSpinner.component';
+
+// type for allImages
+type imgs = Images[];
 
 const Gallery = () => {
-  const [allImages, setAllImages] = useState([]);
-  
+  const [allImages, setAllImages] = useState<imgs | null>(null);
+  const [gotImages, setGotImages] = useState(false);
 
   useEffect(() => {
     axios.get('/images')
-    .then(({data}) => {
-      console.log('images from database', data);
-      setAllImages(data);
-    })
-    .catch((err) => {
-      console.error('error on get images from db client\n', err);
-    })
-  })
+      .then(({ data }) => {
+        console.log('images from database', data);
+        setAllImages(data);
+      })
+      .then(() => {
+        setGotImages(true);
+        console.log('images in the state', allImages);
+      })
+      .catch((err) => {
+        console.error('error on get images from db client\n', err);
+      })
+  }, [gotImages]);
 
   return (
     <div className="container">
       <h1>Gallery</h1>
       <ImagePost />
-      <ImageCard />
+      { allImages ? <ImageCard /> : <loadSpinner /> }
     </div>
   );
 }
