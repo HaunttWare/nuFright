147001133a513b74ef6c79225a930a3d2288161f
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-09 15:29:36 -0500

    feat: created file to define types and props for gallery components

--- client/src/components/Gallery-components/galleryProps.component.tsx
+++ client/src/components/Gallery-components/galleryProps.component.tsx
@@ -1,6 +1,16 @@
 // imports for types
 import { Images } from ".prisma/client";
-
-
-
+//defining types for props
 type imgs = Images[];
+
+//props passed down in gallery components
+interface galleryProps {
+  allImages: imgs,
+  setAllImages?: React.Dispatch<React.SetStateAction<imgs | null>>
+}
+
+
+export {
+  imgs,
+  galleryProps
+}
