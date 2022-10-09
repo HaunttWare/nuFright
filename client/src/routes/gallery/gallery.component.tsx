bb3206a738834a9c39e81c374b8cd450182a3d38
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-08 20:06:19 -0500

    feat: axios request to get all images in database

--- client/src/routes/gallery/gallery.component.tsx
+++ client/src/routes/gallery/gallery.component.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import ImagePost from '../../components/Gallery-components/galleryPost.component';
 
-const Gallery = () => (
-  <div className="container">
-    <h1>Gallery</h1>
-    <ImagePost />
-  </div>
-);
 
+const Gallery = () => {
+  const [allImages, setAllImages] = useState([]);
+  
+
+  useEffect(() => {
+    axios.get('/images')
+    .then(({data}) => {
+      console.log('images from database', data);
+
+    })
+    .catch((err) => {
+      console.error('error on get images from db client\n', err);
+    })
+  })
+
+  return (
+    <div className="container">
+      <h1>Gallery</h1>
+      <ImagePost />
+      
+    </div>
+  );
+}
 export default Gallery;
