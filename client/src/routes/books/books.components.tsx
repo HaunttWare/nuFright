e85f17cb8bcfcfe762720c500195dd895245cb8c
wilsuriel03 <lacurly00@gmail.com>
2022-10-08 22:14:41 -0500

    feat: render booklist

--- client/src/routes/books/books.components.tsx
+++ client/src/routes/books/books.components.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setBookList } from "../../store/books/books.action";
 
-const Books = () => (
-  <div className="container">
-    <h1>Books</h1>
-  </div>
-);
+import BookList from "../../components/book-list/book-list.component";
+
+const Books = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios.get("/books").then(({ data }) => {
+      dispatch(setBookList(data));
+    });
+  }, [dispatch]);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="text-center">Popular Books</h1>
+          <BookList />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Books;
