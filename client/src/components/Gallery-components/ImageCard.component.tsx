9d25c71590aefb6eda42b3835917e6bd5ba26ca3
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-09 17:13:04 -0500

    WIP: working on card sizing

--- client/src/components/Gallery-components/ImageCard.component.tsx
+++ client/src/components/Gallery-components/ImageCard.component.tsx
@@ -5,10 +5,10 @@ const ImageCard = ({ allImages }: galleryProps) => {
   let cTitle = (allImages[0].userId).slice(0, 6);
 
   return (
-    <div className="card">
-      <img src={allImages[0].image} className="card-img-top" alt="..." />
+    <div className="card" >
+      <img src={allImages[0].image} className="card-img-top" alt="..."   />
       <div className="card-body">
-        <h5 className="card-title" >{cTitle}</h5>
+        <h5 className="card-title"  >{cTitle}</h5>
         <p className="card-text" >{ allImages[0].caption }</p>
       </div>
     </div>
