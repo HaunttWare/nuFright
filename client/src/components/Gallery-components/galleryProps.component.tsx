871839dc9dde3fd1d052853bb1d3974b90c3729a
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-09 21:01:12 -0500

    feat: added image type for individual cards

--- client/src/components/Gallery-components/galleryProps.component.tsx
+++ client/src/components/Gallery-components/galleryProps.component.tsx
@@ -1,16 +1,22 @@
 // imports for types
 import { Images } from ".prisma/client";
 //defining types for props
-type imgs = Images[];
+type imageArr = Images[];
+// when passing down individual image
+interface image {
+  image: Images
+}
 
 //props passed down in gallery components
 interface galleryProps {
-  allImages: imgs,
-  setAllImages?: React.Dispatch<React.SetStateAction<imgs | null>>
+  allImages: imageArr,
+  setAllImages?: React.Dispatch<React.SetStateAction<imageArr | null>>,
 }
 
 
+
 export {
-  imgs,
-  galleryProps
+  imageArr,
+  galleryProps,
+  image
 }
