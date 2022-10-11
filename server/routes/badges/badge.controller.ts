c829fa42256b47823de22964c256f49bf708aa02
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-11 16:48:21 -0500

    setup: badge routes

---
+++ server/routes/badges/badge.controller.ts
@@ -0,0 +1,3 @@
+import express, { Request, Response } from 'express';
+import { db } from '../../prisma/utils/db.server';
+
---
+++ server/routes/badges/badge.router.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+const badgeRouter = express.Router();
+
+
+export default badgeRouter;
