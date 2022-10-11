96811f7c2210336337987a45872ce97af83b307a
itcomesinwaves <royce.taylor789@gmail.com>
2022-10-10 19:41:54 -0500

    for real this time

--- client/src/routes/movies-shows/EachMovie.component.tsx
+++ client/src/routes/movies-shows/EachMovie.component.tsx
@@ -40,14 +40,14 @@ const handleLikeSelect = () => {
 
 const handleUnlikeSelect = () => {
 setCheckIsLiked(false);
-  // axios.delete(`/movies/unlike/${movie.id}`, {id: currentUser.id, cinemaId: movie.id})
-//   .then(() => {
-//     setCheckIsLiked(false);
-//   })
-//   .catch((err) => {
-//     console.error(err)
-//   })
- }
+  axios.delete(`/movies/unlike`, {data: {id: currentUser.id, movieId: movie.id}})
+  .then(() => {
+    setCheckIsLiked(false);
+  })
+  .catch((err) => {
+    console.error(err)
+  });
+}
 
 const handleSavedRender = () => {
   setIsSaved(true);
@@ -71,6 +71,13 @@ const isSavedRender = () => {
     return (<button>save</button>)
   }
 };
+// const dislikeRender = () => {
+//   if (isDisliked) {
+//     return (<button>change your mind?</button>)
+//   } else {
+//     return (<button>dislike</button>)
+//   }
+// }
 useEffect(() => {
 
 }, [checkIsLiked])
