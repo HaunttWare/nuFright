e676746112f077fabfc283c134d6fba55488fa55
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-11 16:45:28 -0500

    setup: created dir and files for ratings router

---
+++ server/routes/ratings/rating.controller.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from 'express';
+import { db } from '../../prisma/utils/db.server';
+
+const postRating = (req: Request, res: Response) => {
+  
+}
+
+
---
+++ server/routes/ratings/rating.router.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const ratingRouter = express.Router();
+
+
+
+export default ratingRouter;
