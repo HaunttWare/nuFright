1de9c8c3e30b7f55ace61689d38e76ca89a4bf40
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-12 08:19:24 -0500

    feat: update and delete functions complete, working on postRating, all need to be tested

--- server/routes/ratings/rating.controller.ts
+++ server/routes/ratings/rating.controller.ts
@@ -2,7 +2,58 @@ import express, { Request, Response } from 'express';
 import { db } from '../../prisma/utils/db.server';
 
 const postRating = (req: Request, res: Response) => {
+  const { type } = req.params;
+  const { userId, horrorId, rating } = req.body;
+  let rateData;
+  
+
+}
+
+const updateRating = (req: Request, res: Response) => {
+  const { ratingId, rating } = req.body;
+
+  db.booScale.update({
+    where: {
+      id: ratingId
+    },
+    data: {
+      rating,
+    }
+  })
+  .then((data) => {
+    console.log('updated rating', data);
+    res.sendStatus(200);
+  })
+  .catch((err) => {
+    console.error('error on updating rating\n', err);
+    res.sendStatus(500);
+  })
   
 }
 
 
+
+const deleteRating = (req: Request, res: Response) => {
+  const { ratingId } = req.params
+
+  db.booScale.delete({
+    where: {
+      id: ratingId
+    },
+  })
+  .then((data) => {
+    console.log('deleted rating', data);
+    res.sendStatus(200);
+  })
+  .catch((err) => {
+    console.error('error deleting rating \n', err);
+    res.sendStatus(500);
+  })
+}
+
+
+export {
+  deleteRating,
+  postRating,
+  updateRating
+}
