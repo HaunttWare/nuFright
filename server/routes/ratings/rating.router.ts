a1e7758183fdfccef70cfee2abe23ed90ae91d58
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-12 08:20:36 -0500

    feat: setup routes for post, put, and delete

--- server/routes/ratings/rating.router.ts
+++ server/routes/ratings/rating.router.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import { postRating, updateRating, deleteRating } from './rating.controller';
 
 const ratingRouter = express.Router();
 
+ratingRouter.post('/', postRating);
+ratingRouter.put('/', updateRating);
+ratingRouter.delete('/', deleteRating);
 
 
 export default ratingRouter;
