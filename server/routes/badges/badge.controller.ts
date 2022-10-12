c6deda5aa8788973ce14d00d2b0f572aa2e24d06
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-11 20:06:52 -0500

    feat: starting backend request for badges

--- server/routes/badges/badge.controller.ts
+++ server/routes/badges/badge.controller.ts
@@ -1,3 +1,9 @@
 import express, { Request, Response } from 'express';
 import { db } from '../../prisma/utils/db.server';
 
+const postBadge = (req: Request, res: Response) => {
+  const { userId, badgeName, description, badge } = req.body;
+  // badge var is image source, might be temporary
+
+  
+}
