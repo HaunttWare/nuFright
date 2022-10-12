ba96e657d2473107d4e2c9fe4c9fad8942df5cd4
iamdestinos <iamdestinos@gmail.com>
2022-10-12 15:32:56 -0500

    update: minor style changes to textToSpeech buttons

--- client/src/components/TextToSpeech-components/Voice.component.tsx
+++ client/src/components/TextToSpeech-components/Voice.component.tsx
@@ -6,21 +6,22 @@ const Voice = (props:{lang?:string, text:string}) => {
     const [text, setText] = useState(props.text);
     const [autoplay, setAutoPlay] = useState(false);
     const [isSpeeking, setSpeeking] = useState(false);
-    const [_speech, setSpeech] = useState(new SpeechSynthesisUtterance());
+    const [speech, setSpeech] = useState(new SpeechSynthesisUtterance());
+    speech.onend = () => setSpeeking(false);
 
     useEffect(() => {
         if("speechSynthesis" in window) {
-            _speech.onend = () => setSpeeking(false);
+
         } else {
             setSupported(false);
         }
     }, []);
 
     const play = () => {
-        _speech.text = text;
-        _speech.lang = lang;
+        speech.text = text;
+        speech.lang = lang;
         setSpeeking(true);
-        window.speechSynthesis.speak(_speech);
+        window.speechSynthesis.speak(speech);
     };
 
     const stop = () => {
@@ -28,9 +29,9 @@ const Voice = (props:{lang?:string, text:string}) => {
     };
 
     return (
-        <div id="Voice_Component">
-            <button disabled={isSpeeking} onClick={play}>Play</button>
-            <button disabled={!isSpeeking} onClick={stop}>Stop</button>
+        <div id="Voice_Component" style={{display: 'inline-block'}}>
+            <button disabled={isSpeeking} onClick={play} style={{borderTopLeftRadius: '45%', borderBottomLeftRadius: '45%', minWidth: 43}}>Play</button>
+            <button disabled={!isSpeeking} onClick={stop} style={{borderTopRightRadius: '45%', borderBottomRightRadius: '45%', minWidth: 45}}>Stop</button>
         </div>
     )
 }
