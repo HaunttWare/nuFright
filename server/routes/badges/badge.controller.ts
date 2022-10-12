0d9e0c874fe29c3b9e4696fd6fbadfafc90dd3c5
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-12 00:05:26 -0500

    feat: post request for badges, and unnecessary getUsersBadge request

--- server/routes/badges/badge.controller.ts
+++ server/routes/badges/badge.controller.ts
@@ -3,7 +3,37 @@ import { db } from '../../prisma/utils/db.server';
 
 const postBadge = (req: Request, res: Response) => {
   const { userId, badgeName, description, badge } = req.body;
-  // badge var is image source, might be temporary
+  // badge var is image source, temporary
 
+  db.badges.create({
+    data: {
+      name: badgeName,
+      description,
+      badge,
+      userId,
+    }
+  })
+    .then((data) => {
+      console.log('badge created', data);
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.error('error posting badge\n', err);
+      res.sendStatus(500);
+    })
   
+};
+
+const getUserBadges = (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  db.badges.findMany({
+    where: {
+      userId,
+    }
+  })
+};
+
+export {
+  postBadge
 }
