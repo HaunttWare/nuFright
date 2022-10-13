1f664ff67139b5b0d8c330cc6b00e9c042fe83d0
wilsuriel03 <lacurly00@gmail.com>
2022-10-12 19:10:07 -0500

    feat: rendering users uploaded image

--- client/src/components/profile-tabs/photos-tab/photos-tab.component.tsx
+++ client/src/components/profile-tabs/photos-tab/photos-tab.component.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { ImageData } from "../../../routes/profile/profile.component";
 
-const PhotosTab = () => {
+type PhotosTabProps = {
+  userImages: ImageData[];
+};
+
+const PhotosTab = ({ userImages }: PhotosTabProps) => {
   return (
     <div className="card-body p-4 text-white">
       <div className="d-flex justify-content-between align-items-center mb-4">
@@ -12,36 +17,15 @@ const PhotosTab = () => {
         </p>
       </div>
       <div className="row g-2">
-        <div className="col mb-2">
-          <img
-            src="https://tinyurl.com/4wwv3dxy"
-            alt="image 1"
-            className="w-100 rounded-3"
-          />
-        </div>
-        <div className="col mb-2">
-          <img
-            src="https://tinyurl.com/3m7xcfyy"
-            alt="image 1"
-            className="w-100 rounded-3"
-          />
-        </div>
-      </div>
-      <div className="row g-2">
-        <div className="col">
-          <img
-            src="https://tinyurl.com/4kaap6kv"
-            alt="image 1"
-            className="w-100 rounded-3"
-          />
-        </div>
-        <div className="col">
-          <img
-            src="https://tinyurl.com/452azerx"
-            alt="image 1"
-            className="w-100 rounded-3"
-          />
-        </div>
+        {userImages.map((image) => (
+          <div className="col-6 col-md-4 col-lg-3" key={image.id}>
+            <img
+              src={image.image}
+              alt="user-photo"
+              className="w-100 rounded-3"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
