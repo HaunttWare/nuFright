5a157712cad5c6a557c3a61aba807031fd3bc381
iamdestinos <iamdestinos@gmail.com>
2022-10-13 19:39:06 -0500

    create: costumeslist component

---
+++ client/src/components/Costumes-components/CostumesList.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { selection1 }from './example-data.json';
+
+const CostumesList = () => {
+
+    return (
+        <div>
+            Costumes Page
+            <>
+                {console.log(selection1)}
+            </>
+        </div>
+    )
+}
+
+export default CostumesList;
