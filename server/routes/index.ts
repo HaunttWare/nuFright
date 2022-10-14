9af6f4745abcaa23d66f2a58b491700e797a36b5
wilsuriel03 <lacurly00@gmail.com>
2022-10-14 16:09:04 -0500

    feat: add conversation/message endpoints

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -4,22 +4,26 @@ import authRouter from "./auth/auth.router";
 import userRouter from "./user/user.router";
 import imageRouter from "./images/images.router";
 import MovieRouter from "./movies/movies.router";
-import storyRouter from './stories/stories.router';
+import storyRouter from "./stories/stories.router";
 import booksRouter from "./books/books.router";
 import badgeRouter from "./badges/badge.router";
 import showsRouter from "./shows/shows.router";
+import conversationRouter from "./conversation/conversation.router";
+import messageRouter from "./message/message.router";
 
 const rootRouter = express.Router();
 
 rootRouter.use("/auth", authRouter);
 rootRouter.use("/user", userRouter);
-rootRouter.use('/images', imageRouter);
-rootRouter.use('/movies', MovieRouter);
-rootRouter.use('/story', storyRouter);
-rootRouter.use('/shows', showsRouter);
-rootRouter.use("/books", booksRouter);
-rootRouter.use('/badges', badgeRouter);
 
+rootRouter.use("/conversations", conversationRouter);
+rootRouter.use("/messages", messageRouter);
+
+rootRouter.use("/images", imageRouter);
+rootRouter.use("/movies", MovieRouter);
+rootRouter.use("/story", storyRouter);
+rootRouter.use("/shows", showsRouter);
+rootRouter.use("/books", booksRouter);
+rootRouter.use("/badges", badgeRouter);
 
 export default rootRouter;
-
