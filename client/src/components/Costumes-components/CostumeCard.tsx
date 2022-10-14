c4e78fbd2a25eab9195472c16a923a62a4bea9a2
iamdestinos <iamdestinos@gmail.com>
2022-10-14 17:06:49 -0500

    add: placeholder for invalid images

--- client/src/components/Costumes-components/CostumeCard.tsx
+++ client/src/components/Costumes-components/CostumeCard.tsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 
 const CostumeCard = (props:{name:string, url:string, costumeImg:string, costumeImg_url:string}) => {
     const [isHover, setHover] = useState(false);
+    const replacementImg = require('../../../../assets/Hauntware-logo-prototype.png').default;
     
     return (
         <div className="col-sm-3">
             <div className="card" style={{width: '17rem', background: 'rgb(220, 53, 69)', margin: 5}}>
-                <img className="card-img-top" src={props.costumeImg} alt="Card image cap"></img>
+                <img className="card-img-top" src={!props.costumeImg.includes('https://spirit.scene7.com/is/image/Spirit/') ? replacementImg : props.costumeImg} alt="Card image cap"></img>
                 <div className="card-body">
                     <a className="card-title" href={props.url} target='_blank' rel="noreferrer" style={{textDecoration: 'none', color: 'white', fontWeight: isHover ? 'bold' : 'normal'}} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>{props.name}</a>
                 </div>
