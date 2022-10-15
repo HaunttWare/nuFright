0a7b2f1df4c5a5d4741042a7961bb0c1d90ea0cb
jimenezfede <ricojimenez89@gmail.com>
2022-10-15 14:02:26 -0400

    able to add comment to state

--- client/src/components/comments/comments.tsx
+++ client/src/components/comments/comments.tsx
@@ -1,24 +1,35 @@
 import React, {useMemo, useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import {selectCurrentUser} from '../../store/user/user.selector';
 import axios from 'axios';
 import CommentForm from './comment-form';
 
+interface CommentObj {
+  data: {
+    message: string;
+    userId: string;
+  }
+}
 
 const Comments = (props:any) => {
  
   const [comments, setComments] = useState<string[]>([])
-
+  const {id, name, email, googleId} = useSelector(selectCurrentUser);
+  console.log({id, name})
   // get all the comments with the same type from props
   const options = Object.keys(props);
   // console.log(options)
-  const getComments = () => {
-    
-  }
   
 
   // create a new comment
-  const newComment = () => {
-
+  
+  const newComment = (message: string) => {
+    if (id) {
+      axios.post('/api/comments', {
+        
+      })
+    }
+    setComments(prevComment => [...prevComment, ...message]);
   }
   
 
@@ -26,7 +37,7 @@ const Comments = (props:any) => {
   if (!comments.length) {
     return (
     <>
-      <CommentForm />
+      <CommentForm newComment={newComment} />
     </>)
   } else {
     return (
