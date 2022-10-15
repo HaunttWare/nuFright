1172d40fe50f1ce8e76d157e05e778e4dae04df8
jimenezfede <ricojimenez89@gmail.com>
2022-10-14 20:57:06 -0400

    post request working

--- server/routes/comments/comment.controller.ts
+++ server/routes/comments/comment.controller.ts
@@ -1,37 +1,36 @@
 import {Request, Response} from 'express';
 import {db} from '../../prisma/utils/db.server';
 
-const postComment = async (req: Request, res: Response) => {
-  const {body: {message, category}, params: {type}} = req; 
-  // console.log({message, category, type})
-  interface Comment {
-  
-    message: String;
-    category: String;
-  };
-
-  const data: Comment = {
-
-    message,
-    category,
+const postComment = (req: Request, res: Response) => {
+  const {body: {userId, message, category, horrorId}} = req;
+  const commentObj = {
+    data: {
+      message,
+      userId,
+      cinemaId: null,
+      imagesId: null,
+      bookId: null,
+      storiesId: null
+    }
   }
-  console.log({data})
-  try {
-    // const newComment = await db.comment.create({
-    //   data,
-    //   include: {
-    //     message: true,
-    //     category: true,
-    //   }
-    // })
-    // if (newComment) {
-    //   console.log(newComment);
-    //   res.sendStatus(201);
-    // }
-  } catch(err) {
-    console.log(err);
-    res.sendStatus(500);
+  switch (category) {
+    case "book":
+     commentObj.data.bookId = horrorId
+      break
+    case "cinema":
+     commentObj.data.cinemaId = horrorId
+      break
+    case "images":
+     commentObj.data.imagesId = horrorId
+      break
+    case "stories":
+     commentObj.data.storiesId = horrorId
+      break
   }
+
+  db.comment.create(commentObj)
+  .then(data => res.status(201).json(data))
+  .catch(() => res.sendStatus(500));
 }
 
 export {postComment};
