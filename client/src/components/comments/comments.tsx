cd9c9c49a72e56bee0e2bb69c0607531b6730c8d
jimenezfede <ricojimenez89@gmail.com>
2022-10-15 18:31:04 -0400

    able to post comments to db

--- client/src/components/comments/comments.tsx
+++ client/src/components/comments/comments.tsx
@@ -1,51 +1,64 @@
-import React, {useMemo, useState} from 'react';
+import React, {useMemo, useState, useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {selectCurrentUser} from '../../store/user/user.selector';
 import axios from 'axios';
 import CommentForm from './comment-form';
 
-interface CommentObj {
-  data: {
-    message: string;
-    userId: string;
-  }
-}
+type CommentsData = {
+  bookId: null;
+  cinemaId: null;
+  createdAt: string;
+  id: string;
+  imagesId: null;
+  parentId: null;
+  storiesId: null;
+  updatedAt: string;
+  userId: string;
+  message: string;
+};
 
 const Comments = (props:any) => {
- 
-  const [comments, setComments] = useState<string[]>([])
+  // const [comments, setComments] = useState<string[]>([])
+  const [comments, setComments] = useState<CommentsData[]>([])
   const {id, name, email, googleId} = useSelector(selectCurrentUser);
-  console.log({id, name})
-  // get all the comments with the same type from props
-  const options = Object.keys(props);
-  // console.log(options)
-  
+  const options = Object.keys(props).toString();
+  const horrorId = props[options].id;
+
+  useEffect(() => {
+    axios.get(`/api/comments`)
+      .then(({data}) => {
+        if (data.length) {
+          data.map((comment:any) => {
+            for (const key in comment) {
+              if (comment[key] === horrorId) {
+                setComments((prevComments) => [...prevComments, comment])
+              } 
+            }
+          })
+        }
+      })
+      .catch(err => console.error(err));
+  }, []);
 
   // create a new comment
-  
   const newComment = (message: string) => {
     if (id) {
-      axios.post('/api/comments', {
-        
-      })
+      axios.post(`/api/comments/${horrorId}`, {userId: id, message})
+      .catch(err => console.error(err));
     }
-    setComments(prevComment => [...prevComment, ...message]);
   }
-  
-
-  // check if there are any comments
-  if (!comments.length) {
+  console.log(comments)
+  return comments.length > 0? (
+  <div className='container-fluid'>
+  <h3>Comments</h3>
+  {comments.map((comment: CommentsData, i:number) => {
     return (
-    <>
-      <CommentForm newComment={newComment} />
-    </>)
-  } else {
-    return (
-      <>
-        Comments
-      </>
+      <div key={i}>{comment.message}</div>
     )
-  }
+  })}
+  <CommentForm newComment={newComment} />
+  </div>
+  ) : (<p>no comments</p>);
 }
 
 export default Comments;
