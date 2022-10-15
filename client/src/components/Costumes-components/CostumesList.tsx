39ff08104cde7e51f3b945816f34cc1424046a1c
iamdestinos <iamdestinos@gmail.com>
2022-10-15 10:55:08 -0500

    add: display limited num of costumes and can go through pages

--- client/src/components/Costumes-components/CostumesList.tsx
+++ client/src/components/Costumes-components/CostumesList.tsx
@@ -10,13 +10,30 @@ const CostumesList = () => {
     const [index, setIndex] = useState(0);
     const [listLength, setListLength] = useState(36);
 
+    const switchDisplay = () => {
+        setSelect(!select);
+        setIndex(0);
+    }
+
+    const switchPage = (direction:number = 1) => {
+        if(direction === 1) {
+            setIndex(index + listLength);
+        } else if(direction === -1) {
+            setIndex(index - listLength);
+        }
+    }
+
     return (
         <div>
             <h1>Costumes</h1>
-            <button onClick={() => setSelect(!select)} style={{minWidth: 150, borderRadius: '45%', background: 'black', color: 'lime'}}>{select ? "men's costumes" : "women's costumes"}</button>
+            <button onClick={switchDisplay} style={{minWidth: 150, borderRadius: '45%', background: 'black', color: 'lime'}}>{select ? "men's costumes" : "women's costumes"}</button>
+            <div style={{display: 'inline-flex'}}>
+                <button disabled={!(index > 0)} onClick={() => switchPage(-1)}>Back</button>
+                <button disabled={!(index <= (select ? mens_selection.length : womens_selection.length))} onClick={() => switchPage(1)}>Next</button>
+            </div>
             <div className="row">
-                {(select ? mens_selection : womens_selection).slice(index, listLength).map((costume:any, index:number) => {
-                    return <CostumeCard name={costume.name} url={costume.url} costumeImg={costume.costumeImg} costumeImg_url={costume.costumeImg_url} key={index} />
+                {(select ? mens_selection : womens_selection).map((costume:any, indexKey:number) => {
+                    return index <= indexKey && indexKey < listLength + index && <CostumeCard name={costume.name} url={costume.url} costumeImg={costume.costumeImg} costumeImg_url={costume.costumeImg_url} key={indexKey} />
                 })}
             </div>
         </div>
