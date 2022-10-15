11eb01668a1995866ba7beb4bd2502affb632fe2
iamdestinos <iamdestinos@gmail.com>
2022-10-15 10:26:09 -0500

    add: ability to switch between mens and womens costume cards

--- client/src/components/Costumes-components/CostumesList.tsx
+++ client/src/components/Costumes-components/CostumesList.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import data
 import { selection1 }from './example-data.json';
-import { mens_selection } from './mens-costumes.json';
+import { mens_selection, womens_selection } from './costumes.json';
 //import subcomponents
 import CostumeCard from './CostumeCard';
 
 const CostumesList = () => {
+    const [select, setSelect] = useState(true); //true = men false = women
+    const [index, setIndex] = useState(0);
+    const [listLength, setListLength] = useState(36);
 
     return (
         <div>
             <h1>Costumes</h1>
+            <button onClick={() => setSelect(!select)} style={{minWidth: 150, borderRadius: '45%', background: 'black', color: 'lime'}}>{select ? "men's costumes" : "women's costumes"}</button>
             <div className="row">
-                {mens_selection.map((costume:any, index:number) => {
+                {(select ? mens_selection : womens_selection).slice(index, listLength).map((costume:any, index:number) => {
                     return <CostumeCard name={costume.name} url={costume.url} costumeImg={costume.costumeImg} costumeImg_url={costume.costumeImg_url} key={index} />
                 })}
             </div>
