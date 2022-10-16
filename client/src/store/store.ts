39c68618b4f9ad2af2a2812d71b45f4d5d69302e
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-16 00:09:02 -0500

    added badges to store

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -7,6 +7,7 @@ import { booksReducer } from './books/books.reducer';
 import { moviesReducer } from './movies/movies.reducer';
 import { showsReducer } from './shows/shows.reducer';
 import { ratingsReducer } from './ratings/ratings.reducer';
+import { badgesReducer } from './badges/badges.reducer';
 
 export const store = configureStore({ // <-- this function creates the store 
     reducer: {
@@ -15,6 +16,7 @@ export const store = configureStore({ // <-- this function creates the store
         movies: moviesReducer,
         shows: showsReducer,
         ratings: ratingsReducer,
+        badges: badgesReducer,
     },
     middleware: (process.env.NODE_ENV === 'development' ? [logger] : []) // <-- this adds the logger middleware only in development mode
 });
