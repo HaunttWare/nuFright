4dfa09fb8e0140f42ddc681fd4eeb3e9adf5d67b
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-16 18:54:51 -0500

    dynamic rendering based on whether a user has rated the content or not

--- client/src/components/boo-scale/rating.component.tsx
+++ client/src/components/boo-scale/rating.component.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Unrated from "./unrated.component";
+import Rated from "./rated.component";
 
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../store/user/user.selector";
@@ -9,10 +10,32 @@ import { selectRatingList } from "../../store/ratings/ratings.selector";
 const Rating = (id: { id: string; }) => {
   const currentUser = useSelector(selectCurrentUser);
   const userRatings = useSelector(selectRatingList);
-  const horrorId = id;
+  const [rated, setRated] = useState(false);
+
+  const isRated = () => {
+    if (rated) {
+      return (
+        <Rated id={id.id} />
+      )
+    } else {
+      return (
+        <Unrated id={id.id} />
+      )
+    }
+  }
+
+  useEffect(() => {
+    userRatings.forEach((rateObj: { id: string, rating: number }) => {
+      if (rateObj.id.includes(id.id)) {
+        setRated(true);
+      }
+    })
+  }, [])
 
   return (
-    <Unrated id={id.id} />
+    <div>
+      {isRated()}
+    </div>
   )
 };
 
