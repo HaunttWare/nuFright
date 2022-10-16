eb7bcb608c424e6c31a848cc27dd71806c28e1d4
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-16 00:05:47 -0500

    copied badges store from branch badge alerts

---
+++ client/src/store/badges/badges.action.ts
@@ -0,0 +1,13 @@
+import { BADGES_ACTIONS_TYPES } from "./badges.types";
+import { createAction } from "../utils/createAction";
+
+
+export type badgeData = {
+  name: String;
+  description: String;
+  badge: String;
+};
+
+export const setBadgeList = (badgeList: badgeData[] | [null]) => {
+ return createAction(BADGES_ACTIONS_TYPES.SET_BADGE_LIST, badgeList)
+};
