857398ec75f1b27461f691aab84ef8bd0a1238d1
jimenezfede <ricojimenez89@gmail.com>
2022-10-18 16:42:29 -0400

    deleted unused file

--- client/src/components/comments/commentContext.tsx
+++
@@ -1,9 +0,0 @@
-// import React, {useContext, createContext, useEffect, useMemo, useState} from 'react';
-// import {useParams} from 'react-router-dom';
-
-// const Context = createContext({
-//   currentType: null,
-//   setCurrentType: () => null,
-// });
-
-// export const CommentProvider()
