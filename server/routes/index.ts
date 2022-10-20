994df4468ca242099a0b4590bc7ef1242fa71fdb
wilsuriel03 <lacurly00@gmail.com>
2022-10-20 14:13:08 -0500

    organized routes

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -12,20 +12,24 @@ import badgeRouter from "./badges/badge.router";
 import showsRouter from "./shows/shows.router";
 import PlaylistRouter from "./playlists/playlists.routes";
 import commentRouter from "./comments/comment.router";
+import chatRouter from "./chat/chat.router";
 
 const rootRouter = express.Router();
 
 rootRouter.use("/auth", authRouter);
 rootRouter.use("/user", userRouter);
-rootRouter.use("/images", imageRouter);
+rootRouter.use("/chat", chatRouter);
+
 rootRouter.use("/movies", MovieRouter);
-rootRouter.use("/story", storyRouter);
 rootRouter.use("/shows", showsRouter);
 rootRouter.use("/books", booksRouter);
+rootRouter.use("/images", imageRouter);
+rootRouter.use("/story", storyRouter);
+rootRouter.use("/playlists", PlaylistRouter);
+
 rootRouter.use("/ratings", ratingRouter);
 rootRouter.use("/likes", likesRouter);
 rootRouter.use("/badges", badgeRouter);
 rootRouter.use("/comments", commentRouter);
-rootRouter.use("/playlists", PlaylistRouter);
 
 export default rootRouter;
