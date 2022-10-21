83dac522d731fe2e7f23905f4eb9cb88c2a02ab7
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-10-21 16:33:50 -0500

    fix

--- client/src/components/Stories-components/Stories.tsx
+++ client/src/components/Stories-components/Stories.tsx
@@ -47,7 +47,7 @@ const StoriesPage = () => {
             {view === 'storyList' && <button onClick={() => viewHandler('write', null)} style={{minWidth: 110, maxWidth: 200, background: 'black', color: 'lime', borderRadius: '45%'}}>Write a Story</button>}
             {view === 'storyList' && !allStories.length && <div>Loading...</div>}
             <>
-            {view === 'storyList' && allStories.map(((story:{authorId:String, createdAt:String, id:string, images:any, title:String, story:String, description?:String}, index:any) => {
+            {view === 'storyList' && allStories.map(((story:{authorId:String, createdAt:String, id:String, images:any, title:String, story:String, description?:String}, index:any) => {
                 return <div key={index}>
                         <Story test={viewHandler} story={story} key={index} />
                         <br style={{margin: 5}}></br>
