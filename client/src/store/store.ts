f2826a38b05adadb4855978627c11cede8d14358
wilsuriel03 <lacurly00@gmail.com>
2022-10-22 23:59:21 -0500

    feat: added chat to store

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,26 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger'; 
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
 
+import { userReducer } from "./user/user.reducer";
+import { booksReducer } from "./books/books.reducer";
+import { moviesReducer } from "./movies/movies.reducer";
+import { showsReducer } from "./shows/shows.reducer";
+import { ratingsReducer } from "./ratings/ratings.reducer";
+import { badgesReducer } from "./badges/badges.reducer";
+import { chatReducer } from "./chat/chat.reducer";
 
-import { userReducer } from './user/user.reducer';
-import { booksReducer } from './books/books.reducer';
-import { moviesReducer } from './movies/movies.reducer';
-import { showsReducer } from './shows/shows.reducer';
-import { ratingsReducer } from './ratings/ratings.reducer';
-import { badgesReducer } from './badges/badges.reducer';
-import { chatReducer } from './chat/chat.reducer';
-
-export const store = configureStore({ // <-- this function creates the store 
-    reducer: {
-        user: userReducer,
-        chat: chatReducer,
-        books: booksReducer, 
-        movies: moviesReducer,
-        shows: showsReducer,
-        ratings: ratingsReducer,
-        badges: badgesReducer,
-    },
-    // middleware: (process.env.NODE_ENV === 'development' ? [logger] : []) // <-- this adds the logger middleware only in development mode
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+    chat: chatReducer,
+    books: booksReducer,
+    movies: moviesReducer,
+    shows: showsReducer,
+    ratings: ratingsReducer,
+    badges: badgesReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
-
-
