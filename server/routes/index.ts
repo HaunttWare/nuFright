fca1041e422d834d03823049f31972d2d95dc9a6
wilsuriel03 <lacurly00@gmail.com>
2022-10-23 00:13:44 -0500

    feat: added chat/messages to root router

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,23 +2,25 @@ import express from "express";
 
 import authRouter from "./auth/auth.router";
 import userRouter from "./user/user.router";
-import imageRouter from "./images/images.router";
+import chatRouter from "./chat/chat.router";
+import messageRouter from "./message/message.router";
 import MovieRouter from "./movies/movies.router";
-import storyRouter from "./stories/stories.router";
+import showsRouter from "./shows/shows.router";
 import booksRouter from "./books/books.router";
+import imageRouter from "./images/images.router";
+import storyRouter from "./stories/stories.router";
+import PlaylistRouter from "./playlists/playlists.routes";
 import ratingRouter from "./ratings/rating.router";
 import likesRouter from "./likes/likes.router";
 import badgeRouter from "./badges/badge.router";
-import showsRouter from "./shows/shows.router";
-import PlaylistRouter from "./playlists/playlists.routes";
 import commentRouter from "./comments/comment.router";
-import chatRouter from "./chat/chat.router";
 
 const rootRouter = express.Router();
 
 rootRouter.use("/auth", authRouter);
 rootRouter.use("/user", userRouter);
 rootRouter.use("/chat", chatRouter);
+rootRouter.use("/message", messageRouter);
 
 rootRouter.use("/movies", MovieRouter);
 rootRouter.use("/shows", showsRouter);
