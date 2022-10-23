475fa8b1daf8c06ffbd2194caefb67015f8c5757
wilsuriel03 <lacurly00@gmail.com>
2022-10-23 00:06:06 -0500

    added style provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,11 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
+import { ChakraProvider } from "@chakra-ui/react";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
-
 
 import App from "./App";
 
@@ -19,7 +20,9 @@ const root = ReactDOM.createRoot(el);
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </Provider>
 );
