edf030b2a098a608cad2366f64febc9f05cc28ff
itcomesinwaves <royce.taylor789@gmail.com>
2022-10-24 01:08:00 -0500

    update: tone buttons look like a piano and some styling updates

--- client/src/routes/MusicMaker/MusicMakingView.tsx
+++ client/src/routes/MusicMaker/MusicMakingView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { isDotDotDotToken } from 'typescript';
-import Octave from '../../Octave';
+import Octave from './Octave';
 import RecordingApp from '../../components/record-playback/RecordingApp';
 
 const MusicMakingView = () => {
