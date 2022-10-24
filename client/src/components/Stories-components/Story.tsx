b9ecea229aea8072f4ff6391ca5e7050aad161c8
iamdestinos <iamdestinos@gmail.com>
2022-10-24 17:18:06 -0500

    hookup: like indicator and comment indicator now hooked up to live data

--- client/src/components/Stories-components/Story.tsx
+++ client/src/components/Stories-components/Story.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { textSpanContainsPosition } from 'typescript';
 import "./stories.styles.css";
 
-const Story = (props:{ test:any, story:{createdAt:String, id:String, images:any, title:String, story:String, description?: String}, key:any }) => {
+const Story = (props:{ test:any, story:{createdAt:String, id:String, images:any, title:String, story:String, description?: String, author: {name: string}, likedBy: any, Comment: any}, key:any }) => {
     const HauntedHouse = require('../../../../assets/haunted-house.jpg').default;
 
     return (
@@ -11,7 +11,7 @@ const Story = (props:{ test:any, story:{createdAt:String, id:String, images:any,
             <div className="img-post">
                 <img className="d-block img-fluid" src={props.story.images ? props.story.images : HauntedHouse}  alt="First slide"/>
             </div>
-            <h3><a href="blog-details.html">{props.story.title}</a></h3>
+            <h3><b onClick={() => props.test('story', props.story)}>{props.story.title}</b></h3>
             <p> {props.story.description ? props.story.description : props.story.story.slice(0, 100) + '...'}</p>
         </div>
         <div className="footer">
@@ -19,8 +19,8 @@ const Story = (props:{ test:any, story:{createdAt:String, id:String, images:any,
                 <span onClick={() => props.test('story', props.story)} className="btn btn-outline-secondary">Continue Reading</span>
             </div>
             <ul className="stats">
-                <li><a href="#" className="fa fa-heart">5</a></li>
-                <li><a href="#" className="fa fa-comment">10</a></li>
+                <li><a href="#" className="fa fa-heart">{props.story.likedBy.length}</a></li>
+                <li><a href="#" className="fa fa-comment">{props.story.Comment.length}</a></li>
             </ul>
         </div>
     </div>
