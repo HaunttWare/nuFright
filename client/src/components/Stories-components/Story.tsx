1153886201967e99e763f4d0f1bc255ebd3df502
iamdestinos <iamdestinos@gmail.com>
2022-10-25 10:50:38 -0500

    update: storylist cards changed

--- client/src/components/Stories-components/Story.tsx
+++ client/src/components/Stories-components/Story.tsx
@@ -6,12 +6,14 @@ const Story = (props:{ test:any, story:{createdAt:String, id:String, images:any,
     const HauntedHouse = require('../../../../assets/haunted-house.jpg').default;
 
     return (
-        <div className="card single_post">
+    <div className="card single_post">
+        <div className="top">
+            <h3 style={{marginTop: 5, display: 'flex', justifyContent: 'center', alignItems: 'center'}}><b onClick={() => props.test('story', props.story)} style={{cursor: 'pointer',}}><u>{props.story.title}</u></b></h3>
+        </div>
         <div className="body">
             <div className="img-post">
-                <img className="d-block img-fluid" src={props.story.images ? props.story.images : HauntedHouse}  alt="First slide"/>
+                <img className="d-block img-fluid" onClick={() => props.test('story', props.story)} style={{cursor: 'pointer'}} src={props.story.images ? props.story.images : HauntedHouse}  alt="First slide"/>
             </div>
-            <h3><b onClick={() => props.test('story', props.story)}>{props.story.title}</b></h3>
             <p> {props.story.description ? props.story.description : props.story.story.slice(0, 100) + '...'}</p>
         </div>
         <div className="footer">
@@ -24,7 +26,6 @@ const Story = (props:{ test:any, story:{createdAt:String, id:String, images:any,
             </ul>
         </div>
     </div>
-
     )
 }
 
