5d3526c8b910ea2e4324fc6b3a074a9d0e29ebca
iamdestinos <iamdestinos@gmail.com>
2022-11-01 17:59:57 -0500

    fix: clicking any part of costume card redirects to seller page

--- client/src/components/Costumes-components/CostumeCard.tsx
+++ client/src/components/Costumes-components/CostumeCard.tsx
@@ -12,7 +12,7 @@ const CostumeCard = (props: {
 
 
   return (
-      <div className="card text-white" id="bright" onClick={() => window.open(props.url, "_blank")} style={{cursor: "pointer", margin: 5, minWidth: '13rem', maxWidth: '13rem'}}>
+      <div className="card text-white" id="bright" style={{cursor: "pointer", margin: 5, marginBottom: '1em', minWidth: '13rem', maxWidth: '13rem'}}>
         <div className="image-container">
           <div className="first">
             <div className="d-flex justify-content-between align-items-center">
@@ -25,18 +25,19 @@ const CostumeCard = (props: {
           <img
             src={!props.costumeImg.includes('https://spirit.scene7.com/is/image/Spirit/') ? Math.floor(Math.random() * 1000) + 1 !== 1 ? replacementImg : replacementImg2 : props.costumeImg}
             className="img-fluid rounded thumbnail-image"
+            onClick={() => window.open(props.url, "_blank")}
           />
         </div>
 
         <div className="product-detail-container p-2">
           <div className="d-flex justify-content-between align-items-center">
-            <h5 className="dress-name">{props.name}</h5>
+            <h5 className="dress-name" onClick={() => window.open(props.url, "_blank")} >{props.name}</h5>
           </div>
 
           <div className="d-flex justify-content-between align-items-center pt-1">
 
 
-            <div className="d-flex ">
+            <div className="d-flex " onClick={() => window.open(props.url, "_blank")}>
               <span className="item-size mr-2 btn">S</span>
               <span className="item-size mr-2 btn">M</span>
               <span className="item-size btn">L</span>
@@ -45,7 +46,7 @@ const CostumeCard = (props: {
 
           <div className="d-flex justify-content-between align-items-center pt-1">
 
-            <span className="buy">BUY <i className="fa-solid fa-cart-plus"></i></span>
+            <span className="buy" onClick={() => window.open(props.url, "_blank")}>BUY <i className="fa-solid fa-cart-plus"></i></span>
           </div>
         </div>
       </div>
