ae2997da372dd32b2a077ef8be4a3f7430ef54e0
Federico Jimenez <ricojimenez89@gmail.com>
2022-11-09 16:59:10 -0500

    copied files from upstream

--- server/routes/user/user.routes.ts
+++ server/routes/user/user.routes.ts
@@ -1,30 +1,23 @@
-import express from "express";
-import {
-  getAllUsers,
-  followUser,
-  getFollowers,
-  getFollowing,
-  getUserLikedMovies,
-  getUserLikedShows,
-  getUserSavedMovies,
-  getUserSavedShows,
-  getUserRatingsNBadges,
-} from "./user.controller";
+import express from 'express';
+import {getUser, getUserLikedBooks, getUserLikedMovies, getUserLikedShows, getUsers, getUserSavedMovies, getUserSavedShows, getUserRatingsNBadges} from './user.controller';
 
 const userRouter = express.Router();
 
-userRouter.get("/", getAllUsers);
+// userRouter.get('/:id/liked-books', getUserLikedBooks);
+// userRouter.get('/:id', getUser)
 
-userRouter.post("/follow/:userId", followUser);
-userRouter.get("/followers/:currentUserId", getFollowers);
-userRouter.get("/followings/:currentUserId", getFollowing);
+// get all users
+userRouter.get('/', getUsers);
 
-userRouter.get("/:id/liked-movies", getUserLikedMovies);
-userRouter.get("/:id/liked-shows", getUserLikedShows);
+userRouter.get('/:id/liked-movies', getUserLikedMovies);
 
-userRouter.get("/:id/saved-movies", getUserSavedMovies);
-userRouter.get("/:id/saved-shows", getUserSavedShows);
+userRouter.get('/:id/liked-shows', getUserLikedShows);
 
-userRouter.get("/:id/ratings-badges", getUserRatingsNBadges);
+userRouter.get('/:id/saved-movies', getUserSavedMovies);
+
+userRouter.get('/:id/saved-shows', getUserSavedShows);
+
+userRouter.get('/:id/ratings-badges', getUserRatingsNBadges);
+
 
 export default userRouter;
