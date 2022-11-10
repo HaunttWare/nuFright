29119dfb20d8dadcc18670476c230266fadb8e5e
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-11-09 18:47:10 -0600

    refactor: fixed badgeName property

--- client/src/components/profile-tabs/badges-tab/badges-tab.component.tsx
+++ client/src/components/profile-tabs/badges-tab/badges-tab.component.tsx
@@ -4,10 +4,10 @@ const BadgesTab = ({ userBadges }: any) => {
   return (
     <div className="row mt-5">
         {userBadges.map((badge: any) => (
-            <div className="col-md-4" key={badge.badgeName}>
+            <div className="col-md-4" key={badge.name}>
                 <div className="card text-white bg-dark mb-3">
                     <div className="card-body">
-                        <h5 className="card-title">{badge.badgeName}</h5>
+                        <h5 className="card-title">{badge.name}</h5>
                         <p className="card-text">{badge.description}</p>
                     </div>
                 </div>
