2cef8eef946560a29ef868c4606ff60c405e8d1c
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-11-09 19:12:12 -0600

    rendering badge on badge cards

--- client/src/components/profile-tabs/badges-tab/badges-tab.component.tsx
+++ client/src/components/profile-tabs/badges-tab/badges-tab.component.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 
 const BadgesTab = ({ userBadges }: any) => {
+    const profile = require('../../../../../assets/profile-badge.png').default;
   return (
     <div className="row mt-5">
         {userBadges.map((badge: any) => (
             <div className="col-md-4" key={badge.name}>
                 <div className="card text-white bg-dark mb-3">
+                    <div className="row g-0">
+                        <div className="col-md-4" >
+                    <img src={profile} className="img-fluid rounded-start" alt="" ></img>
+                        </div>
+                        <div className="col-md-8">
                     <div className="card-body">
-                        <h5 className="card-title">{badge.name}</h5>
-                        <p className="card-text">{badge.description}</p>
+                        <h5 className="card-title"><b>{badge.name}</b></h5>
+                        <p className="card-text"><em>{badge.description}</em></p>
+                    </div>
+                        </div>
                     </div>
                 </div>
             </div>
