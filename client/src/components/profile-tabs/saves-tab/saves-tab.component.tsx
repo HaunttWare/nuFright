f0a51cec18441f0cc34facd70e2ce3d6d6694376
wilsuriel03 <lacurly00@gmail.com>
2022-11-13 22:53:48 -0600

    refactor: remove books from categories

--- client/src/components/profile-tabs/saves-tab/saves-tab.component.tsx
+++ client/src/components/profile-tabs/saves-tab/saves-tab.component.tsx
@@ -10,7 +10,6 @@ const SavesTab = () => {
   const [activeTab, setActiveTab] = useState("movies");
   const [userSavedMovies, setUserSavedMovies] = useState([]);
   const [userSavedShows, setUserSavedShows] = useState([]);
-  const [userSavedBooks, setUserSavedBooks] = useState([]);
 
   useEffect(() => {
     // if currentUser is not null, then fetch the data
@@ -43,13 +42,6 @@ const SavesTab = () => {
           >
             Shows
           </button>
-          <button
-            type="button"
-            className="btn btn-outline-secondary"
-            onClick={() => setActiveTab("books")}
-          >
-            Books
-          </button>
         </div>
       </div>
       {/* tab content */}
@@ -59,27 +51,6 @@ const SavesTab = () => {
         )}
         {activeTab === "shows" && (
           <ShowTabContent userSavedShows={userSavedShows} />
-        )}
-        {activeTab === "books" && (
-          <div className="d-flex flex-wrap">
-            <div className="card m-2" style={{ width: "18rem" }}>
-              <img
-                src="https://images.unsplash.com/photo-1542733384-8b3b0f3b5f9d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW92aWV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">Book 1</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            </div>
-          </div>
         )}
       </div>
     </div>
