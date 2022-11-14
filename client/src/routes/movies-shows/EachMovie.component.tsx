b9fe0d5543628552c527a4efc8aece5954ff8017
wilsuriel03 <lacurly00@gmail.com>
2022-11-14 01:25:12 -0600

    fix: cover image and style

--- client/src/routes/movies-shows/EachMovie.component.tsx
+++ client/src/routes/movies-shows/EachMovie.component.tsx
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../store/user/user.selector";
 import { MoviesData } from "../../store/movies/movies.action";
-import Comments from '../../components/comments/comments';
-import Rating from '../../components/boo-scale/rating.component';
-import './movie.styles.scss';
+import Comments from "../../components/comments/comments";
+import Rating from "../../components/boo-scale/rating.component";
+
+import "./movie.styles.scss";
+import { CoverImage } from "./films.styles";
 
 type MovieProp = {
   movie: MoviesData;
@@ -14,9 +16,12 @@ type MovieProp = {
 const EachMovie = ({ movie }: MovieProp) => {
   const currentUser = useSelector(selectCurrentUser);
   const [isLiked, setIsLiked] = useState(false);
-  const [isSaved, setIsSaved] = useState(false);
   const [likeId, setLikeId] = useState("");
-  const [saveId, setSaveId] = useState("");
+  const [showComments, setShowComments] = useState(false);
+
+  const handleShowComments = () => {
+    setShowComments(!showComments);
+  };
 
   const handleLike = () => {
     if (currentUser) {
@@ -34,113 +39,56 @@ const EachMovie = ({ movie }: MovieProp) => {
     }
   };
 
-
-
- const isLikedRender = () => {
-  return isLiked ?
-   (
-    <i className='fa-solid fa-heart' id='unliked-heart' onClick={() => { handleLike() }}></i>
-  )
-  :
-  (
-    <i className='fa-solid fa-heart' style={{color: 'rgba(255, 51, 0, 0.5)'}}id='liked-heart' onClick={() => { handleLike() }}></i>
-  )
- };
-  // const handleSave = () => {
-  //   if (currentUser) {
-  //     axios
-  //       .post(`/api/movies/${movie.id}/save`, {
-  //         userId: currentUser.id,
-  //         savedId: saveId,
-  //         isSaved: !isSaved,
-  //       })
-  //       .then(({ data }) => {
-  //         setSaveId(data.id);
-  //         setIsSaved(!isSaved);
-  //       })
-  //       .catch((err) => console.log(err));
-  //   }
-  // };
-
-  if (currentUser) {
   useEffect(() => {
+    if (!currentUser) return;
 
     if (movie.likedBy) {
       const liked = movie.likedBy.find(
         (like) => like.userId === currentUser.id
-        );
-        if (liked) {
-          setIsLiked(true);
-          setLikeId(liked.id);
-        }
+      );
+      if (liked) {
+        setIsLiked(true);
+        setLikeId(liked.id);
       }
-      if (movie.savedBy) {
-        const saved = movie.savedBy.find(
-          (save) => save.userId === currentUser.id
-          );
-          if (saved) {
-            setIsSaved(true);
-            setSaveId(saved.id);
-          }
-        }
-      }, [movie, currentUser.id]);
     }
+  }, [movie, currentUser.id]);
 
   return (
-    <div className='movie_card' id='bright'>
-            <div className='info_section'>
-              <div className='movie_header'>
-                <h1>{movie.title}</h1>
-                <img className='locandina' src={movie.images} />
-                <img className='locandina-big' src={movie.images} />
-                {/* <h4>add director and year here</h4> */}
-                {/* <span className='minutes'>add movie length here</span> */}
-                <p className='type'>{movie.genres}</p>
-              </div>
-              <div className='movie_desc'>
-                <p className='text'>{movie.description}</p>
-              </div>
-              <div className='movie_social'>
-                <ul>
-                  <li>
-                    {/* <i className='fa-solid fa-share-nodes'></i> */}
-                  </li>
-                  <li>
-                    { isLikedRender() }
-                  </li>
-                  <li>
-                    {/* <i className='fa-solid fa-message'></i> */}
-                  </li>
-                </ul>
-
-                <Comments category={movie} type={'cinema'} />
-                <Rating id={movie.id} type={'cinema'} />
-              </div>
-            </div>
-            <div className='blur_back bright_back'></div>
+    <>
+      <div className="movie_card" id="bright">
+        <div className="info_section">
+          <div className="movie_header">
+            <img className="locandina" src={movie.images} />
+            <h1>{movie.title}</h1>
+            <p className="type">{movie.genres}</p>
           </div>
-    );
-  };
-  
-  export default EachMovie;
-    // <div className="col-md-3 col-sm-6">
-    //   <div className="card" >
-    //     <img src={movie.images} className="card-img-top" alt="..." />
-    //     <div className="card-body">
-    //       <h5 className="card-title">{movie.title}</h5>
-    //       {/* movie genre */}
-    //       <p className="card-text">
-    //         <small className="text-muted">{movie.genres}</small>
-    //       </p>
-    //       <p className="card-text">{movie.description}</p>
-    //       <button className="btn btn-secondary" onClick={handleLike}>
-    //         {isLiked ? "Unlike" : "Like"}
-    //       </button>
-    //       <button className="btn btn-secondary" onClick={handleSave}>
-    //         {isSaved ? "Unsave" : "Save"}
-    //       </button>
-    //       <Rating id={movie.id} type={'cinema'} />
-    //       <Comments category={movie} type={'cinema'} />
-    //     </div>
-    //   </div>
-    // </div>
+          <div className="movie_desc">
+            <p className="text">{movie.description}</p>
+          </div>
+          <Rating id={movie.id} type={"cinema"} />
+          <div className="movie_social">
+            <ul>
+              <li>
+                <i
+                  className="fa-solid fa-heart"
+                  onClick={handleLike}
+                  style={{ color: isLiked ? "red" : "" }}
+                ></i>
+              </li>
+              <li>
+                <i
+                  className="fa-solid fa-message"
+                  onClick={handleShowComments}
+                ></i>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <CoverImage image={movie.images} />
+      </div>
+      {showComments && <Comments category={movie} type={"cinema"} />}
+    </>
+  );
+};
+
+export default EachMovie;
--- client/src/routes/movies-shows/EachShow.component.tsx
+++ client/src/routes/movies-shows/EachShow.component.tsx
@@ -1,22 +1,25 @@
 import axios from "axios";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../store/user/user.selector";
 import { ShowData } from "../../store/shows/shows.action";
-import Comments from '../../components/comments/comments';
-import Rating from '../../components/boo-scale/rating.component';
-import { selectCurrentShows } from "../../store/shows/shows.selector";
+import Comments from "../../components/comments/comments";
+import Rating from "../../components/boo-scale/rating.component";
+
+import { CoverImage } from "./films.styles";
 
 type ShowProp = {
   show: ShowData;
 };
 const EachShow = ({ show }: ShowProp) => {
   const currentUser = useSelector(selectCurrentUser);
-  const currentShows = useSelector(selectCurrentShows);
   const [isLiked, setIsLiked] = useState(false);
-  const [isSaved, setIsSaved] = useState(false);
   const [likeId, setLikeId] = useState("");
-  const [saveId, setSaveId] = useState("");
+  const [showComments, setShowComments] = useState(false);
+
+  const handleShowComments = () => {
+    setShowComments(!showComments);
+  };
 
   const handleLike = () => {
     axios
@@ -32,107 +35,53 @@ const EachShow = ({ show }: ShowProp) => {
       .catch((err) => console.log(err));
   };
 
-  // const handleSave = () => {
-  //   axios
-  //     .post(`/api/shows/${show.id}/save`, {
-  //       userId: currentUser.id,
-  //       savedId: saveId,
-  //       isSaved: !isSaved,
-  //     })
-  //     .then(({ data }) => {
-  //       setSaveId(data.id);
-  //       setIsSaved(!isSaved);
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
-  const isLikedRender = () => {
-    return isLiked ?
-    (
-     <i className='fa-solid fa-heart' onClick={() => { handleLike() }}></i>
-   )
-   :
-   (
-     <i className='fa-solid fa-heart' style={{color: 'purple'}} onClick={() => { handleLike() }}></i>
-   )
-  }
-
   useEffect(() => {
+    if (!currentUser) return;
     if (show.likedBy) {
-      const liked = show.likedBy.find(
-        (like) => like.userId === currentUser.id
-      );
+      const liked = show.likedBy.find((like) => like.userId === currentUser.id);
       if (liked) {
         setIsLiked(true);
         setLikeId(liked.id);
       }
     }
-    if (show.savedBy) {
-      const saved = show.savedBy.find(
-        (save) => save.userId === currentUser.id
-      );
-      if (saved) {
-        setIsSaved(true);
-        setSaveId(saved.id);
-      }
-    }
   }, [show, currentUser.id]);
 
   return (
-    <div className='movie_card' id='bright'>
-    <div className='info_section'>
-      <div className='movie_header'>
-        <img className='locandina' src={show.images} />
-        <h1>{show.title}</h1>
-        {/* <h4>add director and year here</h4> */}
-        {/* <span className='minutes'>add show length here</span> */}
-        <p className='type'>{show.genres}</p>
+    <>
+      <div className="movie_card" id="bright">
+        <div className="info_section">
+          <div className="movie_header">
+            <img className="locandina" src={show.images} />
+            <h1>{show.title}</h1>
+            <p className="type">{show.genres}</p>
+          </div>
+          <div className="movie_desc">
+            <p className="text">{show.description}</p>
+          </div>
+          <Rating id={show.id} type={"cinema"} />
+          <div className="movie_social">
+            <ul>
+              <li>
+                <i
+                  className="fa-solid fa-heart"
+                  onClick={handleLike}
+                  style={{ color: isLiked ? "red" : "" }}
+                ></i>
+              </li>
+              <li>
+                <i
+                  className="fa-solid fa-message"
+                  onClick={handleShowComments}
+                ></i>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <CoverImage image={show.images} />
       </div>
-      <div className='movie_desc'>
-        <p className='text'>{show.description}</p>
-      </div>
-      <div className='movie_social'>
-        <ul>
-          <li>
-            {/* <i className='fa-solid fa-share-nodes'></i> */}
-          </li>
-          <li>
-            { isLikedRender() }
-            {/* <i className='fa-solid fa-heart'></i> */}
-          </li>
-          <li>
-            {/* <i className='fa-solid fa-message'></i> */}
-          </li>
-        </ul>
-        <Comments category={show} type={'cinema'} />
-        <Rating id={show.id} type={'cinema'} />
-      </div>
-    </div>
-    <div className='blur_back bright_back'></div>
-  </div>
-)
+      {showComments && <Comments category={show} type={"cinema"} />}
+    </>
+  );
 };
-    
-export default EachShow;
-    // <div className="col-md-3 col-sm-6">
-    //   <div className="card">
-    //     <img src={show.images} className="card-img-top" alt="..." />
-    //     <div className="card-body">
-    //       <h5 className="card-title">{show.title}</h5>
-    //       <p className="card-text">
-    //         <small className="text-muted">{show.genres}</small>
-    //       </p>
-    //       <p className="card-text">{show.description}</p>
-    //       <button className="btn btn-secondary" onClick={handleLike}>
-    //         {isLiked ? "Unlike" : "Like"}
-    //       </button>
-    //       <button className="btn btn-secondary" onClick={handleSave}>
-    //         {isSaved ? "Unsave" : "Save"}
-    //       </button>
-    //       <Rating id={show.id} type={'cinema'} />
-    //       <Comments category={show} type={'cinema'} />
-    //     </div>
-    //   </div>
-    // </div>
-  
-  
 
+export default EachShow;
