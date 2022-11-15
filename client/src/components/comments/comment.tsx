761e5980b381d25c74989164294cf9f13ec04ab1
Federico Jimenez <ricojimenez89@gmail.com>
2022-11-14 23:48:22 -0600

    lined up username and date and made name bold

--- client/src/components/comments/comment.tsx
+++ client/src/components/comments/comment.tsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import CommentForm from './comment-form';
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../store/user/user.selector";
 
@@ -22,9 +21,6 @@ const dateFormatter = new Intl.DateTimeFormat(undefined, {
 
 const Comment = ({ comment, deleteComment }: CommentProps) => {
   const [users, setUsers] = useState<Users>([]);
-  const [edit, setEdit] = useState(false);
-  const [reply, setReply] = useState(false);
-  const [message, setMessage] = useState<string>();
   const currentUser = useSelector(selectCurrentUser);
 
   useEffect(() => {
@@ -40,13 +36,13 @@ const Comment = ({ comment, deleteComment }: CommentProps) => {
 
   return (
     <>
-    <div>
-      <div className='header'>
-        <span className='row'>{user?.name}</span>
+    <div className='comment container'>
+      <div className='row mb-5'>
+        <span className='col'><b>{user?.name}</b></span>
         <span className='col'>
           {dateFormatter.format(Date.parse(comment.createdAt))}
         </span>
-        <div className='message'>{comment.message}</div>
+        <p className='message'>{comment.message}</p>
         <div className='footer'>
         {(currentUser ? currentUser.id === comment.userId : false) && <i className="fa-solid fa-trash" id={comment.id} onClick={e => { if(currentUser) { if(currentUser.id === comment.userId) {deleteComment(e)}}}}></i>}
         </div>
